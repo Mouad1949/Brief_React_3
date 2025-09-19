18848bdecc4fa3169e20ee0a4321286081c27558
M.Laalam <153172784+Mouad1949@users.noreply.github.com>
2025-09-19 14:53:48 +0100

    Update ComponentUserCard.jsx

--- src/Componenet/ComponentUserCard.jsx
+++ src/Componenet/ComponentUserCard.jsx
@@ -8,7 +8,6 @@ function ComponentUserCard({user}) {
     <div className='m-4'>
         <img src={user.image} alt='Image user' className='w-[80px] h-[70px] mx-auto'/>
         <h6 className='text-center mt-4'>{user.name}</h6>
-        <p>progress :{Math.round(progress)}%</p>
         <div className="w-full bg-gray-200 rounded-full h-2.5">
           <div className="bg-blue-600 h-2.5 rounded-full" style={{width: `${Math.round(progress)}%`}}></div>
         </div>
